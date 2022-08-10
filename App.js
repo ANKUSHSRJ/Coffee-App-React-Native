2e1ae2ddd33fee9eda52c2f4dde0fff47f21a5d6
Ankush SRJ <69152440+ANKUSHSRJ@users.noreply.github.com>
2022-08-10 18:43:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,49 @@
+import { View, Text , Image, Button} from 'react-native'
+import React from 'react'
+import SignIn from './src/Screens/SignIn/SignIn'
+import HomeScreen from './src/Screens/HomeScreen/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import logo from './assets/logo.png'
+
+const Stack = createStackNavigator();
+function LogoTitle() {
+  return (
+    <View  >
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require('./assets/logo.png')}
+    />
+    </View>
+  );
+}
+
+
+const App = () => {
+  return (
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignIn">
+
+        <Stack.Screen name="HomeScreen" 
+        component={HomeScreen}   
+        options={{headerTitle: () => (
+          <Image source={logo} resizeMode="contain" style={{width: 100, height:100}} />
+        ),
+        headerTitleAlign: 'center',
+        headerLeft: ()=> null,  
+         }}
+  
+   
+        />
+        <Stack.Screen name="SignIn" component={SignIn} options={{   headerTitle: props => <LogoTitle {...props} /> }} 
+        />
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  )
+}
+
+export default App
